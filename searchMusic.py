dda3b288f8183a6774b8aa415e1f1754403d1695
prateek_vij <prateekvij55@gmail.com>
2016-02-23 00:58:06 +0530

    Change searchMusic file. Store list of songs in an array.

--- searchMusic.py
+++ searchMusic.py
@@ -7,4 +7,16 @@ sample_input=sample_input.lower()
 gutter=["play ","music ","song ","listen ","hear "];
 for gt in gutter:
 	sample_input=sample_input.replace(gt,'');
-print sample_input;
+
+music_dir="~/Music/English\ Music"
+
+#Storing the list of songs extracted from music folder in an array
+cmd="ls "+music_dir
+music_files = subprocess.Popen(cmd,  shell=True, stdout=subprocess.PIPE) #runnning the subprocess
+temp = music_files.stdout.read() #string of filenames
+
+fileraw=temp.split("\n")
+filenames=[]
+for name in fileraw:
+	filenames.append(name.lower())
+print filenames
