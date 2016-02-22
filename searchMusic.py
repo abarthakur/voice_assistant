6928d4811ec05fcf4733280e060bfc4a309e4376
prateek_vij <prateekvij55@gmail.com>
2016-02-23 01:04:16 +0530

    Change searchMusic file. Search the list of songs for maximum match

--- searchMusic.py
+++ searchMusic.py
@@ -30,3 +30,28 @@ for name in filenames:
 	count=count+1
 
 print filekeys
+
+pointer=-1;
+select_ptr=-1;
+top_words=0
+temp_words=0
+top_score=0
+temp_score=0
+
+
+for file in filekeys:
+	pointer+=1
+	for inp in in_splits:
+		if inp in file:
+			temp_score=temp_score+len(inp)
+			temp_words+=1
+	if temp_score >= top_score:
+		selected=file
+		top_score=temp_score
+		top_words=temp_words
+		select_ptr=pointer
+
+	temp_score=0
+	temp_words=0
+out_len=len(selected);
+print selected
