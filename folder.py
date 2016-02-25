ab87041481ad251ff46eba32c158e6b841050270
hritik <hritik@iitg.ernet.in>
2016-02-26 01:11:35 +0530

    Fix the bug of client home address

--- folder.py
+++ folder.py
@@ -1,7 +1,10 @@
 import os
 cmd=raw_input("Enter the command: ")
+
 folder="Desktop"
-for root,dirs,files in os.walk("/home/hritik"):
+home= os.path.expanduser("~")
+#http://stackoverflow.com/questions/18860679/searching-a-directory-for-folders-and-files-using-python\
+for root,dirs,files in os.walk(home):
 	for name in dirs:
 		if '.' not in os.path.join(root,name) and (name.lower()).endswith(folder.lower()):
 			print os.path.join(root,name)
