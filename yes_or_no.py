e9605b32bdad9b4d29e2bb53934cdca973c5fa94
hritik <hritik@iitg.ernet.in>
2016-04-01 02:22:31 +0530

    Create warning dialogue box

---
+++ yes_or_no.py
@@ -0,0 +1,8 @@
+import Tkinter
+from Tkinter import *
+import tkMessageBox
+
+#if you want to show yes or no
+ans= tkMessageBox.askyesno("Message you want to write at the top","Message you want to write at window")
+#True for Yes button and vice versa
+print ans
