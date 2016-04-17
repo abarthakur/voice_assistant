84e0b6478b2fa2db270587441f03c2fe8e609e3a
rahul.kant <rahulkant333@gmail.com>
2016-04-17 19:49:59 +0530

    Modify Firefox Module. Implement  functionalities using xdotools after testing

--- src/firefox.py
+++ src/firefox.py
@@ -2,7 +2,6 @@
 # This module is responsible for controlling your Browser Completely
 
 import os 
-from pykeyboard import PyKeyboard
 import time
 
 def start_window(): # Start Firefox
@@ -12,156 +11,98 @@ def search(term): # Search a particular term on Default Search Engine
 	os.system('firefox -search '+term)
 
 def close_window(): # Close the Firefox Window
-	os.system('wmctrl -c "Mozilla Firefox"')
+	os.system('xdotool search --name "Mozilla Firefox" windowkill')
 
 def maximize(): # Maximise the Firefox Window
-	os.system('wmctrl -R "Mozilla Firefox" ')
+	os.system('xdotool search --name Mozilla windowactivate')
+	cmd="xdotool key ctrl+super+Up"
+	os.system(cmd)
 
 def minimize(): # Minimise the Firefox Window
-	os.system('wmctrl -R "Mozilla Firefox"')
-	kboard = PyKeyboard()
-	kboard.press_key(kboard.alt_key)
-	kboard.tap_key(' ')
-	kboard.release_key(kboard.alt_key)
-	time.sleep(.1)
-	kboard.tap_key(kboard.down_key)
-	kboard.tap_key(kboard.enter_key)
+	os.system('xdotool search --name "Mozilla Firefox" windowminimize')
+	
 
 def new_tab(): # Open a New Tab
-	os.system('wmctrl -R "Mozilla Firefox"')
-	kboard = PyKeyboard()
-	kboard.press_key(kboard.control_l_key)
-	kboard.tap_key('t')
-	kboard.release_key(kboard.control_l_key)
+	os.system('xdotool search --name "Mozilla Firefox" windowactivate')
+	os.system('xdotool key "ctrl+t"')
  
 def close_tab(): # Close the current Tab
-	os.system('wmctrl -R "Mozilla Firefox"')
-	kboard = PyKeyboard()
-	kboard.press_key(kboard.control_l_key)
-	kboard.tap_key(kboard.function_keys[4])
-	kboard.release_key(kboard.control_l_key)
+	os.system('xdotool search --name "Mozilla Firefox" windowactivate')
+	os.system('xdotool key "ctrl+F4"')
 
 def next_tab(): # Navigate to tab on the right
-	os.system('wmctrl -R "Mozilla Firefox"')
-	kboard = PyKeyboard()
-	kboard.press_key(kboard.control_l_key)
-	kboard.tap_key(kboard.tab_key)
-	kboard.release_key(kboard.control_l_key)
+	os.system('xdotool search --name "Mozilla Firefox" windowactivate')
+	os.system('xdotool key "ctrl+Tab"')
+	
 
 def previous_tab(): # Navigate to tab on left
-	os.system('wmctrl -R "Mozilla Firefox"')
-	kboard = PyKeyboard()
-	kboard.press_key(kboard.control_l_key)
-	kboard.press_key(kboard.shift_key)
-	kboard.tap_key(kboard.tab_key)
-	kboard.release_key(kboard.shift_key)
-	kboard.release_key(kboard.control_l_key)
-
+	os.system('xdotool search --name "Mozilla Firefox" windowactivate')
+	os.system('xdotool key "ctrl+shift+Tab"')
+	
 def scroll_down(): # Scroll Down
-	os.system('wmctrl -R "Mozilla Firefox"')
-	kboard = PyKeyboard()
-	kboard.tap_key(kboard.down_key)
-	kboard.tap_key(kboard.down_key)
+	os.system('xdotool search --name "Mozilla Firefox" windowactivate')
+	os.system('xdotool key Down')
+	
 
 def scroll_up(): # Scroll Up
-	os.system('wmctrl -R "Mozilla Firefox"')
-	kboard = PyKeyboard()
-	kboard.tap_key(kboard.up_key)
-	kboard.tap_key(kboard.up_key)
-
+	os.system('xdotool search --name "Mozilla Firefox" windowactivate')
+	os.system('xdotool key Up')
+	
 def search_page(string): # Search for a particular term in the page
-	os.system('wmctrl -R "Mozilla Firefox"')
-	kboard = PyKeyboard()
-	kboard.press_key(kboard.control_l_key)
-	kboard.tap_key('f')
-	kboard.release_key(kboard.control_l_key)
-	time.sleep(.1)
-	kboard.type_string(string)
+	os.system('xdotool search --name "Mozilla Firefox" windowactivate')
+	os.system('xdotool key a')
 
 def zoom_in(): # Zoom in the current tab
-	os.system('wmctrl -R "Mozilla Firefox"')
-	kboard = PyKeyboard()
-	kboard.press_key(kboard.control_l_key)
-	kboard.tap_key('+')
-	kboard.release_key(kboard.control_l_key)
+	os.system('xdotool search --name "Mozilla Firefox" windowactivate')
+	os.system('xdotool key Ctrl+plus')	
 
 def zoom_out(): # Zoom out the current tab
-	os.system('wmctrl -R "Mozilla Firefox"')
-	kboard = PyKeyboard()
-	kboard.press_key(kboard.control_l_key)
-	kboard.tap_key('-')
-	kboard.release_key(kboard.control_l_key)
-
-
+	os.system('xdotool search --name "Mozilla Firefox" windowactivate')
+	os.system('xdotool key Ctrl+minus')
+	
 def forward():
-	os.system('wmctrl -R "Mozilla Firefox"')
-	kboard = PyKeyboard()
-	kboard.press_key(kboard.alt_l_key)
-	kboard.tap_key(kboard.right_key)
-	kboard.release_key(kboard.alt_l_key)
-
+	os.system('xdotool search --name "Mozilla Firefox" windowactivate')
+	os.system('xdotool key alt+Right')
+	
 def back():
-	os.system('wmctrl -R "Mozilla Firefox"')
-	kboard = PyKeyboard()
-	kboard.press_key(kboard.alt_l_key)
-	kboard.tap_key(kboard.left_key)
-	kboard.release_key(kboard.alt_l_key)
-
+	os.system('xdotool search --name "Mozilla Firefox" windowactivate')
+	os.system('xdotool key alt+Left')
+	
 def refresh():
-	os.system('wmctrl -R "Mozilla Firefox"')
-	kboard = PyKeyboard()
-	kboard.tap_key(kboard.function_keys[5])
-
+	os.system('xdotool search --name "Mozilla Firefox" windowactivate')
+	os.system('xdotool key F5')
+	
 def reload():
-	os.system('wmctrl -R "Mozilla Firefox"')
-	kboard = PyKeyboard()
-	kboard.press_key(kboard.control_l_key)
-	kboard.tap_key(kboard.function_keys[5])
-	kboard.release_key(kboard.control_l_key)
+	os.system('xdotool search --name "Mozilla Firefox" windowactivate')
+	os.system('xdotool key ctrl+F5')
+	
 
 def bookmark(): # Adds a bookmark
-	os.system('wmctrl -R "Mozilla Firefox"')
-	kboard = PyKeyboard()
-	kboard.press_key(kboard.control_l_key)
-	kboard.tap_key('d')
-	kboard.release_key(kboard.control_l_key)
-	kboard.tap_key(kboard.enter_key)
+	os.system('xdotool search --name "Mozilla Firefox" windowactivate')
+	os.system('xdotool key ctrl+d')
 
 def open_bookmark(): # Open the bookmark Window
-	os.system('wmctrl -R "Mozilla Firefox"')
-	kboard = PyKeyboard()
-	kboard.press_key(kboard.control_l_key)
-	kboard.press_key(kboard.shift_key)
-	kboard.tap_key('o')
-	kboard.release_key(kboard.shift_key)
-	kboard.release_key(kboard.control_l_key)
+	os.system('xdotool search --name "Mozilla Firefox" windowactivate')
+	os.system('xdotool key ctrl+shift+o')
 	
 def history(): # Display the History window
-	os.system('wmctrl -R "Mozilla Firefox"')
-	kboard = PyKeyboard()
-	kboard.press_key(kboard.control_l_key)
-	kboard.tap_key('h')
-	kboard.release_key(kboard.control_l_key)
+	os.system('xdotool search --name "Mozilla Firefox" windowactivate')
+	os.system('xdotool key ctrl+h')
+	
 
 def delete_history(): # Delete the History
-	os.system('wmctrl -R "Mozilla Firefox"')
-	kboard = PyKeyboard()
-	kboard.press_key(kboard.control_l_key)
-	kboard.press_key(kboard.shift_key)
-	kboard.tap_key(kboard.delete_key)
-	kboard.release_key(kboard.shift_key)
-	kboard.release_key(kboard.control_l_key)
+	os.system('xdotool search --name "Mozilla Firefox" windowactivate')
+	os.system('xdotool key ctrl+shift+Delete')
+	
 
 def print_page(): # Prompt the user to Print Window
-	os.system('wmctrl -R "Mozilla Firefox"')
-	kboard = PyKeyboard()
-	kboard.press_key(kboard.control_l_key)
-	kboard.tap_key('p')
-	kboard.release_key(kboard.control_l_key)
+	os.system('xdotool search --name "Mozilla Firefox" windowactivate')
+	os.system('xdotool key ctrl+p')
+	
 
 def save_page(): # Prompt the user to save window
-	os.system('wmctrl -R "Mozilla Firefox"')
-	kboard = PyKeyboard()
-	kboard.press_key(kboard.control_l_key)
-	kboard.tap_key('s')
-	kboard.release_key(kboard.control_l_key)
+	os.system('xdotool search --name "Mozilla Firefox" windowactivate')
+	os.system('xdotool key ctrl+s')
+	
+
+save_page()
