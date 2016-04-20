a86a06e2e3a81fd9fb0b731c0337f219b3311511
Aneesh Barthakur <a.barthakur@iitg.ernet.in>
2016-04-21 02:23:26 +0530

    Changed init method so that mic and recognizer get instantiated there itself. Also suppress_ALSA_warnings on init. Refactored test and speech2text fns to reflect that

--- src/speech2text.py
+++ src/speech2text.py
@@ -9,17 +9,18 @@ class Listener(threading.Thread):
 		threading.Thread.__init__(self)
 		self.threadID = threadID
 		self.name = name
-		#self.ALSA_err_handler=self.suppress_ALSA_warnings()
-		#self.recognizer=gsr.Recognizer()
-		#self.mic = gsr.Microphone()
-		# self.counter = counter
-	
+		self.ALSA_err_handler=self.suppress_ALSA_warnings()
+		self.recognizer=gsr.Recognizer()
+		self.mic = gsr.Microphone()
+
+#need to reset error handler at time of exiting thread			
 	def run(self):
 		print "Starting " + self.name
 		self.test()
 		print "Exiting " + self.name
 
 #http://stackoverflow.com/questions/7088672/pyaudio-working-but-spits-out-error-messages-each-time
+
 	def suppress_ALSA_warnings(self):
 		ERROR_HANDLER_FUNC = CFUNCTYPE(None, c_char_p, c_int, c_char_p, c_int, c_char_p)
 		c_error_handler = ERROR_HANDLER_FUNC(self.py_error_handler)
@@ -32,28 +33,33 @@ class Listener(threading.Thread):
 		pass
 
 
-
-	def speech2text(self,r,m):
+	def speech2text(self):
 		try: 
 			print "Say Something ..."
-			with m as source: audio = r.listen(source=source,timeout=2)
-			value = r.recognize_google(audio_data=audio,language="en-IN")
+			with self.mic as source: 
+				audio = self.recognizer.listen(source=source,timeout=2)
+			
+			value = self.recognizer.recognize_google(audio_data=audio,language="en-IN")
 			return format(value).encode("utf-8")
+		
 		except gsr.UnknownValueError:
 			print("Oops! Didn't catch that")
 		except gsr.RequestError as e:
 			print("Uh oh! Couldn't request results from Google Speech Recognition service; {0}".format(e))
 
+
 	def test(self):
-		handler=self.suppress_ALSA_warnings()
-		r = gsr.Recognizer()
-		m = gsr.Microphone()
+		# handler=self.suppress_ALSA_warnings()
+		# r = gsr.Recognizer()
+		# m = gsr.Microphone()
 		print "Silence for 5 seconds"
-		with m as source: r.adjust_for_ambient_noise(source=source,duration=5)
+		with self.mic as source: self.recognizer.adjust_for_ambient_noise(source=source,duration=5)
 		print "Ok, good to go!"
-		while(True):
+		i=0
+		while(i<5):
 			try:
-				print self.speech2text(r,m)
+				print self.speech2text()
+				i+=1
 			except gsr.WaitTimeoutError:
 				pass
 
