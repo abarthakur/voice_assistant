9a17ef3bece365e69072c7698286b15665c28973
Aneesh Barthakur <a.barthakur@iitg.ernet.in>
2016-04-21 03:09:01 +0530

    implemented push_to_queue fn which can push to multithreaded queue for inter-thread communication. Changed the init args to include the workQueue and Lock objects (Lock is a primitive lock object used to synchronize communications between threads). Wrote the basic code of reading the queue from main thread as well(now commented out).

--- src/speech2text.py
+++ src/speech2text.py
@@ -2,16 +2,21 @@ import speech_recognition as gsr
 import os
 from ctypes import *
 import threading
+import Queue
 
 class Listener(threading.Thread):
 
-	def __init__(self, threadID, name):
+	#Should all other fns besides recalibrate be private? perhaps we can even set recalibrate to be private by using
+	#threading.Event
+	def __init__(self, threadID, name, queueLock,workQueue):
 		threading.Thread.__init__(self)
 		self.threadID = threadID
 		self.name = name
 		self.ALSA_err_handler=self.suppress_ALSA_warnings()
 		self.recognizer=gsr.Recognizer()
 		self.mic = gsr.Microphone()
+		self.queueLock=queueLock
+		self.workQueue=workQueue
 
 #need to reset error handler at time of exiting thread			
 	def run(self):
@@ -52,6 +57,10 @@ class Listener(threading.Thread):
 		with self.mic as source: self.recognizer.adjust_for_ambient_noise(source=source,duration=5)
 		print "Ok, good to go!"
 
+	def push_to_queue(self,item):
+		self.queueLock.acquire()
+		self.workQueue.put(item=item,block=False)
+		self.queueLock.release()
 
 	def test(self):
 		# handler=self.suppress_ALSA_warnings()
@@ -61,13 +70,24 @@ class Listener(threading.Thread):
 		i=0
 		while(i<5):
 			try:
-				print self.speech2text()
+				text_output= self.speech2text()
+				self.push_to_queue(text_output)
 				i+=1
 			except gsr.WaitTimeoutError:
 				pass
 
-# Create new threads
-thread1 = Listener(1, "Thread-1")
-# Start new Threads
-thread1.start()
-print "Exiting Main Thread"
+# queueLock = threading.Lock()
+# workQueue = Queue.Queue(10)
+# # Create new threads
+# thread1 = Listener(1, "Thread-1",queueLock,workQueue)
+# # Start new Threads
+# thread1.start()
+# out_file = open("out.txt","a")
+# while(thread1.is_alive()):
+# 	try:
+# 		text = workQueue.get(True)
+# 		out_file.write(text+"\n")
+# 		print "From main thread:"+text
+# 	except Queue.Empty:
+# 		continue
+# print "Exiting Main Thread"
