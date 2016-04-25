357f3615c2ebeb44180a33446dbb7eb425baa6ad
prateek_vij <prateekvij55@gmail.com>
2016-04-26 02:42:54 +0530

    Create a Regex matcher for unresolved results

--- src/decider.py
+++ src/decider.py
@@ -1,14 +1,14 @@
 import os
 from os import system
-
+import re
 
 def generate(features):
 	f_verb=features['verb']
 	f_obj=features['object']
+	task={}
 	with open("function_map") as fmap:
-		task={}
 		for line in fmap:
-			line=line[:-1]
+			line=line.strip()
 			terms=line.split()
 			if f_verb==terms[0] and f_obj==terms[1]:
 				task['module']=terms[2]
@@ -18,8 +18,29 @@ def generate(features):
 				else:
 					task['param']=[]
 	return task
-print generate({'verb':'listen','object':'song'})
 
 
-
-	
+def resolve(features):
+	task = {}
+	templates = []
+	with open("regex.txt") as regex:
+		for line in regex:
+			line=line.strip()
+			template=line
+			templates.append(template)
+	string=features['input'].lower()
+	for t in templates:
+		t=t.split("###")
+		template=t[0]
+		try:
+			if (re.match(template, string)):
+				task['module']=t[1]
+				task['func']=t[2]
+				if len(t)>3:
+					task['param']=terms[3:]
+				else:
+					task['param']=[]
+				return  task
+		except:
+			print "Sorry Didn't get you"
+	return task
